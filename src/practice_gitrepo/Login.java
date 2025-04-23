6b5b055da843f704461d36e3809e164b8a325ac9
SouravLab <souravmishra612@gmail.com>
2025-04-24 00:31:47 +0530

    update in master branch for log in method

--- src/practice_gitrepo/Login.java
+++ src/practice_gitrepo/Login.java
@@ -4,4 +4,7 @@ public class Login {
     public void performLogin() {
         System.out.println("User logged in successfully!");
     }
+    public void statusCheck() {
+    	System.out.println("statusCheck method created");
+    }
 }
