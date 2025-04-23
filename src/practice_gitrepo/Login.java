38aee969385af52b351f1468a81a9cacf89ea853
SouravLab <souravmishra612@gmail.com>
2025-04-24 00:29:33 +0530

    login method addeda new method

--- src/practice_gitrepo/Login.java
+++ src/practice_gitrepo/Login.java
@@ -4,4 +4,8 @@ public class Login {
     public void performLogin() {
         System.out.println("User logged in successfully!");
     }
+    private void changePwd() {
+		// TODO Auto-generated method stub
+
+	}
 }
