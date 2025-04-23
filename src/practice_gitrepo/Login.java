ed07f85032ec841a3e6d74ae0965356ad450435a
SouravLab <souravmishra612@gmail.com>
2025-04-24 00:10:25 +0530

    rename project

---
+++ src/practice_gitrepo/Login.java
@@ -0,0 +1,7 @@
+package practice_gitrepo;
+
+public class Login {
+    public void performLogin() {
+        System.out.println("User logged in successfully!");
+    }
+}
---
+++ src/practice_gitrepo/Logout.java
@@ -0,0 +1,7 @@
+package practice_gitrepo;
+
+public class Logout {
+    public void performLogout() {
+        System.out.println("User logged out successfully!");
+    }
+}
